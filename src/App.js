4bd25c07fb5b2a7d34501b0cc72e38df7152b8be
Amit Singh Rawat <50137861+rawatamit1234@users.noreply.github.com>
2025-05-26 11:13:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonial from "./components/Testimonial";
 import Contact from "./components/Contact";
-import Counter from "./components/Counter"
 function App() {
   return (
     <div className="App">
